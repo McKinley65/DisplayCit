e2356aca0b460fe7142471c2b3cc62644c6e964c
n8x0b7T <deretilium@protonmail.com>
2022-01-29 18:33:06 -0500

    news

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
 const express = require('express')
-const app = express()
+const Parser = require('rss-parser');
+const request = require('request');
 
-let Parser = require('rss-parser');
+const app = express()
 let parser = new Parser();
+
+var API_KEY = '6d4345254a23efccfb861669c6f141cc';
+const newRSS = "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml"
+
+
+// let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`
+
+
+// request(url, { json: true }, (error, res, body) => {
+//   if (error) {
+//     return console.log(error)
+//   };
+
+//   if (!error && res.statusCode == 200) {
+//     console.log(body.main.temp)
+//     console.log(body.weather[0].main)
+
+//     sendData.weather = { "temp": body.main.temp, "description": body.weather[0].main }
+//     res.send(sendData)
+
+//   };
+// })
+
+
+// Function call
+// forecast("greenville");
+
+
 
 
 app.get('/', function (req, res) {
-
-  (async () => {
-    let feed = await parser.parseURL('https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml');
-
-    console.log(feed.items[2].title)
-    res.send(feed.items[2].title)
-
-  })();
+  parser.parseURL(newRSS, function (err, feed) {
+    res.json({
+      "news": feed.items[2].title,
+      "weather": {
+        "tempurature": "",
+        "description": ""
+      }
+    });
+  })
 })
 
-
-
-const request = require('request');
-var API_KEY = '6d4345254a23efccfb861669c6f141cc';
-
-const forecast = function (city) {
-
-// var url = `http://api.openweathermap.org/data/2.5/weather?`
-// 			+`lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
-
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`
-
-	request({ url: url, json: true }, function (error, response) {
-		if (error) {
-			console.log('Unable to connect to Forecast API');
-		}
-		else {
-      console.log(response)
-
-			// console.log('It is currently '
-			// 	+ response.body.main.temp
-			// 	+ ' degrees out.'
-			// );
-
-			// console.log('The high today is '
-			// 	+ response.body.main.temp_max
-			// 	+ ' with a low of '
-			// 	+ response.body.main.temp_min
-			// );
-
-			// console.log('Humidity today is '
-			// 	+ response.body.main.humidity
-			// );
-		}
-	})
-}
-
-var latitude = 22.7196; // Indore latitude
-var longitude = 75.8577; // Indore longitude
-
-// Function call
-forecast("greenville,sc");
 
 
 app.listen(3000)
