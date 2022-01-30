b903e208082283cf2b41e95099f1bd88622a9e63
hazelwood-dal <73398226+hazelwood-dal@users.noreply.github.com>
2022-01-30 10:18:36 -0500

    news

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,7 +1,21 @@
 window.onload = function(){
 
  fetch("/api/weather")
-.then(x => console.log(x.text()))
+.then(x => console.log())
+
+
+updateUI({
+"weather":{temp: "50", describe:""},
+"news": "this is the newadlkfja asklf kajsdf lkasdjf laks"
+
+})
+}
+
+
+var updateUI = function(x){
+    document.getElementById("weathertext").innerText= x.weather.temp
+
+    document.getElementById("news").innerText= x.news
 
 
 }
